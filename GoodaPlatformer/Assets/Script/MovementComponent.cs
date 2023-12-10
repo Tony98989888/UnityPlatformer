8db80ee95d394d3c33e876ed0241acdd127ccb3c
Tony <zhuyouxu666@outlook.com>
2023-12-10 19:39:26 +0800

    Add walljump

--- GoodaPlatformer/Assets/Script/MovementComponent.cs
+++ GoodaPlatformer/Assets/Script/MovementComponent.cs
@@ -1,4 +1,6 @@
+using System.Data;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 using UnityEngine.InputSystem;
 
 public class MovementComponent : MonoBehaviour
@@ -32,6 +34,13 @@ public class MovementComponent : MonoBehaviour
 
     [Header("Facing")] bool m_isFacingRight;
 
+    [Header("Wall Jump")] private bool m_isWallJumping;
+    float m_wallJumpDirection;
+    float m_wallJumpTime;
+    float m_wallJumpTimer;
+    Vector2 m_wallJumpForce;
+    
+
     [SerializeField] Rigidbody2D m_rb;
 
 
@@ -64,6 +73,7 @@ public class MovementComponent : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext context)
     {
+        // Normal jump
         if (m_isOnGround)
         {
             if (context.performed)
@@ -76,6 +86,12 @@ public class MovementComponent : MonoBehaviour
                 // Button is not pressed
                 m_rb.velocity = new Vector2(m_rb.velocity.x, m_rb.velocity.y * m_jumpDecreaseFactor);
             }
+        }
+        // Wall Jump when not on ground
+        if (context.performed && m_wallJumpTimer > 0f)
+        {
+            m_isWallJumping = true;
+            m_rb.velocity = new Vector2(m_wallJumpDirection * m_wallJumpForce.x, m_wallJumpForce.y);
         }
     }
 
@@ -129,6 +145,24 @@ public class MovementComponent : MonoBehaviour
             localScale.x *= -1f;
             transform.localScale = localScale;
         }
+    }
+
+    void UpdateWallJump()
+    {
+        if (m_isWallSliding)
+        {
+            m_isWallJumping = false;
+            m_wallJumpDirection = -transform.localScale.x;
+            m_wallJumpTimer = m_wallJumpTime;
+        }else if (m_wallJumpTimer > 0f)
+        {
+            m_wallJumpTimer -= Time.deltaTime;
+        }
+    }
+
+    void CancelWallJump()
+    {
+        m_isWallJumping = false;    
     }
 
     void OnDrawGizmosSelected()
